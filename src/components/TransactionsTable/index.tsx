4c7f5aa8891c904764d7a429c5e42c797a3ccf6d
Leonardo Blauth <leonardo.blauth@dealerup.com.br>
2022-09-25 19:46:46 -0300

    Configure axios

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,13 +1,13 @@
 import { useEffect } from "react";
-import axios from "axios";
+import { api } from "../../services/api";
 
 import { Container } from "./styles";
 
 export function TransactionTable() {
   useEffect(() => {
-    axios
-      .get('http://localhost:3000/api/transactions')
-      .then(({data}) => console.log(data))
+    api
+      .get('transactions')
+      .then(response => console.log(response.data))
   }, [])
   return (
     <Container>
