b243a7de2d349d44c0cb9971d54d64d516985854
Leonardo Blauth <leonardo.blauth@dealerup.com.br>
2022-09-25 19:41:46 -0300

    Add and config miragejs

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
+import axios from "axios";
+
 import { Container } from "./styles";
 
 export function TransactionTable() {
+  useEffect(() => {
+    axios
+      .get('http://localhost:3000/api/transactions')
+      .then(({data}) => console.log(data))
+  }, [])
   return (
     <Container>
       <table>
