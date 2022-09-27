02ec6fce0262c63376184e779e805b7129bdb262
Leonardo Blauth <leonardo.blauth@dealerup.com.br>
2022-09-26 21:22:51 -0300

    Map transaction into table

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,29 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: uuidv4(),
+          title: "Freelance de website",
+          type: "deposit",
+          category: "Dev",
+          amount: 6000,
+          createdAt: new Date('2022-09-12 09:00:00')
+        },
+        {
+          id: uuidv4(),
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Casa",
+          amount: 1100,
+          createdAt: new Date('2022-09-14 11:00:00')
+        },
+      ],
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
